f7645ee14367d1cab2a7ea8d4af375b866d70129
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-09 22:58:39 +0200

    impostazione file

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- logo -->
+    <div>
+      <img src="" alt="" />
+    </div>
+
+    <nav>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
